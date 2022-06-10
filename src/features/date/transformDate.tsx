9f6c36b4f2c365237c758165303caf12e609b889
Maciej Przybylski <m.przybylski3@gmail.com>
2022-06-10 23:47:02 +0200

    fix: change transformed date presentation

--- src/features/date/transformDate.tsx
+++ src/features/date/transformDate.tsx
@@ -1,9 +1,8 @@
 export const transformDate = (date: string | Date | undefined): string => {
   if (typeof date === 'undefined') return '';
 
-  let newDate = '';
   const dateObject = new Date(date);
+  const result = dateObject.toLocaleDateString('en-GB');
 
-  newDate = `${dateObject.getDate()}/${dateObject.getMonth()}/${dateObject.getFullYear()}`;
-  return newDate;
+  return result;
 };
